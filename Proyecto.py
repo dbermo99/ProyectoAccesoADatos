da702e085e89fb82580f5c166c54b45321883768
dbermo99 <72387622+dbermo99@users.noreply.github.com>
2021-11-15 17:55:37 +0100

    Update Proyecto.py
    alta y modificacion

--- Proyecto.py
+++ Proyecto.py
@@ -1,1 +1,69 @@
-pruebaaa
+from typing import Text
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import Element, SubElement, Comment, ElementTree
+
+def prettify(elem):
+    from xml.etree import ElementTree
+    from xml.dom import minidom
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ElementTree.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
+
+def leerXML(fichero):
+    arbol = ET.parse(fichero)
+    root = arbol.getroot()
+    return root
+
+def buscar(lib,tit):
+    pos = 0
+    for libro in lib:
+        if(libro[0].text==tit):
+            return pos
+        pos += 1
+    return -1
+
+def alta():
+    tree = leerXML("Libreria.xml")
+
+    titulo = input("Titulo\n")
+    autor = input("Autor\n")
+    genero = input("Genero\n")
+    anio = input("Año de publicacion\n")
+
+    libro = ET.Element('libro')
+    tit = ET.SubElement(libro, 'titulo')
+    tit.text = titulo
+    aut = ET.SubElement(libro, 'autor')
+    aut.text = autor
+    gen = ET.SubElement(libro, 'genero')
+    gen.text = genero
+    a = ET.SubElement(libro, 'anio')
+    a.text = anio
+    tree.append(libro)
+
+    salida = prettify(tree)
+    file = open("Libreria.xml","w")
+    file.write(salida)
+    file.close()
+
+def modificar():
+    tree = leerXML("Libreria.xml")
+    libro = input("Titulo del libro a modificar")
+    anio = input("Introduce el nuevo año de publicacion:\n")
+    pos = buscar(tree,libro)
+    if(pos!=-1):
+	    tree[pos][3].text = str(anio)
+    else:
+	    print("Libro no encontrado")
+    salida = prettify(tree)
+    file = open("Libreria.xml","w")
+    file.write(salida)
+    file.close()
+
+print("EMPEZAMOS")
+
+alta()
+
+print("FIN")
